7e14e6e8febf0b93111677b14f414e2ba5a62f8b
zahirbegulic@gmail.com <zahirbegulic@gmail.com>
2021-03-18 08:42:42 +0100

    hasOwnProperty method

---
+++ ex09/src/index.js
@@ -0,0 +1,7 @@
+function myFunction(myObj, checkProp){
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    }
+    return "Not Found";
+}
+console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title"));
