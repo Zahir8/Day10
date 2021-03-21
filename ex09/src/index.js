70b019e448a6d9e797df5bb6853dd82c91b902c7
Zahir Begulić <46743547+Zahir8@users.noreply.github.com>
2021-03-21 23:38:46 +0100

    edited file

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,7 +1,13 @@
-function myFunction(myObj, checkProp){
-    if (myObj.hasOwnProperty(checkProp)) {
+function myFunction(myObj, checkProp) {
+    if (myObj.hasOwnProperty(checkProp) === true) {
         return myObj[checkProp];
+    } else {
+        return "Not found";
     }
-    return "Not Found";
 }
-console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title"));
+console.log(myFunction({title: "Titanic", song: "My heart will go on", genre: "drama"}, "title"));
+console.log(myFunction({title: "Titanic", song: "My heart will go on", genre: "drama"}, "song"));
+console.log(myFunction({title: "Titanic", song: "My heart will go on", genre: "drama"}, "genre"));
+console.log(myFunction({title: "Titanic", song: "My heart will go on", genre: "drama"}, "actor"));
+
+module.exports = myFunction;
