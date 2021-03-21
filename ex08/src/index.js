c6ebd53c94183e403925cb08691d7e1e71e7f982
Zahir Begulić <46743547+Zahir8@users.noreply.github.com>
2021-03-21 23:37:06 +0100

    Update index.js
    edited file

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,16 +1,16 @@
-function getUserName(email){
-    var username = "";
+function getUsername(email) {
+    var username = '';
+
     var username = {
-        "user1@gmail.com": "user1",
-        "user2@gmail.com": "user2",
-        "user3@gmail.com": "user3",
-        "user4@gmail.com": "user4",
-        "user5@gmail.com": "user5",
-        "user6@gmail.com": "user6"
-    }
+        "user1@mail.com": "user1",
+        "user2@mail.com": "user2",
+        "user3@mail.com": "user3",
+        "user4@mail.com": "user4",
+        "user5@mail.com": "user5",
+        "user6@mail.com": "user6"
+    };  
     username = username[email];
-    return username;   
+    return username;
 }
-console.log(getUserName("user1@gmail.com"));
-getUserName("user1@gmail.com");
-module.exports = getUserName;
+console.log(getUsername("user2@mail.com"));
+module.exports = getUsername;
