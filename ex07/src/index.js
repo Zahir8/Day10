6e8c269db1e121f689a46ad4905c6f9932cdeb73
Zahir Begulić <46743547+Zahir8@users.noreply.github.com>
2021-03-21 23:35:11 +0100

    Update index.js
    edited file

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,13 +1,14 @@
-var lion = {
-    name = "Simba",
-    legs = 4,
-    tails = 1,
-    roar = "roar-roar"
-};
-function myFunction(property){
-    
-    delete lion[property];
+function myFunction(a) {
+    var lion = {
+        name: "Simba",
+        legs: 4,
+        tails: 1,
+        roar: "roar-roar"
+    }
+    delete lion["roar"];
     return lion;
+    
 }
-console.log(myFunction("roar"));
+
 myFunction("roar");
+module.exports = myFunction;
